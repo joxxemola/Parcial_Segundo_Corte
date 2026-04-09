54d2e6f5613eef17cb75061032dbd2b4e0542b58
Jose D Molano P <jdmolanoperdomo@gmail.com>
2026-04-09 18:24:02 -0500

    Commit

---
+++ src/main/java/com/mycompany/parcial_segundo_corte/Parcial_Segundo_Corte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.parcial_segundo_corte;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Parcial_Segundo_Corte {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
